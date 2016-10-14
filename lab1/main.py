fa063295a102e5017f3a7b0dee3f2c558dbdbedb
enricosuria <suria.enrico@hotmail.com>
2016-10-14 23:54:09 +0200

    es1 completato

--- lab1/main.py
+++ lab1/main.py
@@ -1,63 +1,90 @@
 #!/usr/bin/python
 
 from calculator import Calculator
+import json
 
 if __name__ == "__main__":
 	my_calc_1 = Calculator("calculator_1");
 
 ans=True
+print ("\n\n\t--------> PYTHON CALCULATOR PLUS <--------\n")
+print ("\t------------> Powered by E&E <------------")
 while ans:
-    input_string=raw_input("\n\tInsert <command> <operator1> <operator2> ... <operatorN> :\n\t") 
+    input_string=raw_input("\n\n\tInsert <command> <operator1> <operator2> ... <operatorN> :\n\t") 
     input_list = input_string.split(' ')
    # input_list = input_list.reverse()
     ans=input_list.pop(0)
-    print ans
-    op1=float(input_list[1])
-    op2=float(input_list[2])
+    obj={}
+    obj["command"] = ans
+    op=[]
+    n=len(input_list)
+    for x in xrange(0,n):
+    	op.insert(x,float(input_list.pop(0)))
+    
 
     if ans=="add": 
-      print("\n ADDITION") 
-      result = my_calc_1.add(op1,op2)
+      print("\nADDITION") 
+      result=op.pop(0)
+      obj["op1"] = result
+      for x in xrange(0,n-1):
+      	temp_str="op"+str(x+2)
+      	temp_op = op.pop(0)
+      	obj[temp_str]=temp_op
+      	result=my_calc_1.add(result,temp_op)
       print "\t%s: the result is %.2f \n" % (my_calc_1.getID(), result)
+      obj["result"] = result
+      output_json=json.dumps(obj)
+      print output_json
+
 
     elif ans=="sub":
-      print("\n SUBSTRACTION") 
-      result = my_calc_1.sub(op1,op2)
+      print("\nSUBSTRACTION") 
+      result=op.pop(0)
+      obj["op1"] = result
+      for x in xrange(0,n-1):
+      	temp_str="op"+str(x+2)
+      	temp_op = op.pop(0)
+      	obj[temp_str]=temp_op
+      	result=my_calc_1.sub(result,temp_op)
       print "\t%s: the result is %.2f \n" % (my_calc_1.getID(), result)
+      obj["result"] = result
+      output_json=json.dumps(obj)
+      print output_json
+
 
     elif ans=="mul":
-      print("\n MULTIPLICATION") 
-      result = my_calc_1.mul(op1,op2)
+      print("\nMULTIPLICATION") 
+      result=op.pop(0)
+      obj["op1"] = result
+      for x in xrange(0,n-1):
+      	temp_str="op"+str(x+2)
+      	temp_op = op.pop(0)
+      	obj[temp_str]=temp_op
+      	result=my_calc_1.mul(result,temp_op)
       print "\t%s: the result is %.2f \n" % (my_calc_1.getID(), result)
+      obj["result"] = result
+      output_json=json.dumps(obj)
+      print output_json
+
 
     elif ans=="div":
-      print("\n DIVISION") 
+      print("\nDIVISION") 
       try:
-      	result = my_calc_1.div(op1,op2)
+      	result=op.pop(0)
+      	obj["op1"] = result
+      	for x in xrange(0,n-1):
+      	  temp_str="op"+str(x+2)
+      	  temp_op = op.pop(0)
+      	  obj[temp_str]=temp_op
+      	  result=my_calc_1.div(result,temp_op)
       except ZeroDivisionError:
       	print "\tCan't divide by zero"
       else:
       	print "\t%s: the result is %.2f \n" % (my_calc_1.getID(), result)
+      obj["result"] = result
+      output_json=json.dumps(obj)
+      print output_json
+
 
     elif ans !="":
-      print("\n\tNot Valid Choice Try again") 
-
-
-
-	# result = my_calc_1.add()
-	# print "%s: the result for the addition is %f" % (my_calc_1.getID(), result)
-
-	# result = my_calc_1.add(3)
-	# print "%s: the result for the addition is %.2f" % (my_calc_1.getID(), result)
-
-	# result = my_calc_1.add(3, 4.457)
-	# print "%s: the result for the addition is %.3f" % (my_calc_1.getID(), result)
-
-
-	# print "---"
-	# my_calc_2 = Calculator("I_am_different!!");
-	# result = my_calc_2.add(12, 1.23)
-	# print "%s: the result for the addition is %.2f" % (my_calc_2.getID(), result)
-
-
-
+      print("\n\tNot Valid Choice Try again")
