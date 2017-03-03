7e8657ce991d4e237400f0308f8d1a8dc7c2fc11
eros93 <filippi.eros@gmail.com>
2017-03-03 11:59:01 +0100

    telegramBot commit 03_03_17

---
+++ _Progetto_/TelegramBot/MQTT_pub_sub.py
@@ -0,0 +1,35 @@
+
+import paho.mqtt.client as mqtt
+import json
+
+class MySubscriber():
+
+	def __init__(self, clientid):
+		self.client_mqtt = mqtt.Client(clientid, False)
+
+		self.client_mqtt.on_connect = self.myOnConnect
+		self.client_mqtt.on_message = self.myOnMessage
+
+
+	def myOnConnect(self, clientid, userdata, flags, rc):
+		print ("\n\tSubscriber with clientid: "+str(clientid))
+		print ("\n\tConnected to message Broker with result code (rc): "+str(rc))
+
+	def myOnMessage(self, clientid, userdata, msg):
+		#print ("\n\tMessage: "+msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+		self.message = json.loads(msg.payload)
+
+	def mySubscribe(self, topic, qos=2):
+		#POSSIBLE ERROR-CHECK
+		self.client_mqtt.subscribe(topic, qos)
+
+	def myUnsubscribe(self, topic):
+		#POSSIBLE ERROR-CHECK
+		self.client_mqtt.unsubscribe(topic)
+
+	def start(self, broker, port):
+		self.client_mqtt.connect(broker,port)
+		self.client_mqtt.loop_start()
+
+	def stop(self):
+		self.client_mqtt.loop_stop()
--- _Progetto_/TelegramBot/main.py
+++ _Progetto_/TelegramBot/main.py
@@ -1,63 +1,101 @@
 from telegramBot import TelegramBot
+from MQTT_pub_sub import MySubscriber
 import time
+import datetime
+import requests
+import json
 
 TOKEN = "294196450:AAEv98AIlpUQ9Kb6QXD-RelIr2QVh8PI6YM"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+res_cat_ip = "192.168.1.70"
+res_cat_port = "8080"
 
-def handle_updates(bot, updates) :
+
+
+def handle_updates(bot, updates, subscriber) :
     for update in updates["result"]:
         text = update["message"]["text"]
         chat = update["message"]["chat"]["id"]
+        item = ['Water Temperature', 'Ground Humidity', 'Weather Forecast']
+        keyboard = Bot.build_keyboard(item)
         if text == "/start":
-            item = []
-            item = ['water Temperature', 'ground Umidity', 'weather information']
-            keyboard = Bot.build_keyboard(item)
             bot.send_message("Welcome to your personal SeeUlater_Bot",chat,keyboard)
-        elif text == "water Temperature":
-            tmp = 20
-            text = "the water temperature is %r degree " % tmp
+        elif text == "Water Temperature":
+            text = "Please wait....\nRetrieving information...."
             bot.send_message(text,chat)
-        elif text == "ground Umidity":
-            tmp = 20
-            text = "the water temperature is %r degree " % tmp
+            (weather_topic, mqtt_topic) = topic_update(res_cat_ip, res_cat_port)
+            subscriber.mySubscribe(mqtt_topic[0])
+            time.sleep(1)
+            obj = subscriber.message
+            subscriber.myUnsubscribe(str(mqtt_topic[0]))
+            date = datetime.datetime.fromtimestamp(obj["timestamp"]).strftime('%d/%m/%Y %H:%M')
+            text = "On %s the water temperature was %r degree " % (str(date), obj["temp"])
+            bot.send_message(text,chat,keyboard)
+        elif text == "Ground Humidity":
+            text = "Please wait....\nRetrieving information...."
             bot.send_message(text,chat)
-        elif text == "weather information":
-            tmp = 20
-            text = "the water temperature is %r degree " % tmp
+            (weather_topic, mqtt_topic) = topic_update(res_cat_ip, res_cat_port)
+            subscriber.mySubscribe(mqtt_topic[0])
+            time.sleep(1)
+            obj = subscriber.message
+            subscriber.myUnsubscribe(str(mqtt_topic[0]))
+            date = datetime.datetime.fromtimestamp(obj["timestamp"]).strftime('%d/%m/%Y %H:%M')
+            text = "At %s the ground moisture was %r " % (str(date), obj["hum_gr"]) + "%"
+            bot.send_message(text,chat,keyboard)
+        elif text == "Weather Forecast":
+            text = "Please wait....\nRetrieving information...."
             bot.send_message(text,chat)
+            (weather_topic, mqtt_topic) = topic_update(res_cat_ip, res_cat_port)
+            subscriber.mySubscribe(weather_topic)
+            time.sleep(1)
+            obj = subscriber.message
+            subscriber.myUnsubscribe(str(weather_topic))
+            time_sunset = datetime.datetime.fromtimestamp(obj["sunset"]).strftime('%H:%M')
+            time_sunrise = datetime.datetime.fromtimestamp(obj["sunrise"]).strftime('%H:%M')
+            text = "The weather forecast for tomorrow:\n- Sunrise will be at %s\n- Sunset will be at %s\n- Precipitation probability is %r\n- Precipitation intensity will be is %r mm\n\nSummary: %s" % (str(time_sunrise),str(time_sunset), obj["precipProb"], obj["precipInt"], str(obj["summary"]))
+            bot.send_message(text,chat,keyboard)
         elif text.startswith("/"):
             continue
         else:
-            message = "comando non esistente"
+            message = "Not valid Command"
             bot.send_message(message, chat)
+def broker_info (res_cat_ip, res_cat_port):
+    res_url = "http://" + res_cat_ip + ":" + res_cat_port + "/res_cat/broker_info"
+    response_res_cat = requests.get(res_url)
+    obj = json.loads(response_res_cat.text)
+    broker_ip = obj["broker_ip"]
+    broker_port = obj["broker_port"]
+    return broker_ip, broker_port
+
+
+def topic_update (res_cat_ip, res_cat_port):
+    res_url = "http://" + res_cat_ip + ":" + res_cat_port + "/res_cat/all"
+    response_res_cat = requests.get(res_url)
+    obj = json.loads(response_res_cat.text)
+    weather_topic = obj["weath_mqtt_out_topic"]
+    int = len(obj["dev_list"])
+    mqtt_topic = []
+    for i in range(int):
+        if obj["dev_list"][i]["mqtt_role"] == "p":
+            mqtt_topic.append(obj["dev_list"][i]["mqtt_topic"])
+    return weather_topic, mqtt_topic
 
 
 Bot = TelegramBot(URL)
 
-#(text_rx,chatID)=Bot.get_last_chat_id_and_text()
-#text_tx = "ciao dal bot"
-#Bot.send_message(text_tx,chatID)
+(broker_ip, broker_port) = broker_info(res_cat_ip, res_cat_port)
 
-Bot.prettyprint_json(Bot.get_updates())
+sub = MySubscriber("Botsub")
+sub.start(broker_ip, broker_port)
 
 last_update_id = None
 while True:
     updates = Bot.get_updates(last_update_id)
-    Bot.prettyprint_json(updates)
     if len(updates["result"]) > 0:
         last_update_id = Bot.get_last_update_id() + 1
-        Bot.prettyprint_json(updates)
-        handle_updates(Bot,updates)
+        #Bot.prettyprint_json(updates)
+        handle_updates(Bot, updates, sub)
     time.sleep(0.5)
 
 
 
-
-#while True:
-#    updateID =Bot.get_last_update_id()
-#    updates = Bot.get_updates(updateID)
-#    if len(updates["result"]) > 0:
-#        handle_updates(Bot)
-#    time.sleep(0.5)
-
-#Bot.prettyprint_json(Bot.send_message(text_tx,chatID))
