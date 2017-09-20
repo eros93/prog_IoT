a58352230382f2fa4a2d1771cb48b9df4241e183
enricosuria <suria.enrico@hotmail.com>
2017-09-20 22:03:28 +0200

    Little update Withdraw 20092017

--- _Progetto_/Withdraw Control/main.py
+++ _Progetto_/Withdraw Control/main.py
@@ -5,10 +5,10 @@ import time
 import schedule
 from datetime import datetime
 
-RESOURCECAT_IP = "192.168.1.71"
+RESOURCECAT_IP = "192.168.1.73"
 RESOURCECAT_PORT = 8080
 
-myWithdraw = Withdraw("WithDraw1")
+myWithdraw = Withdraw("WithdrawRasp3")
 
 def periodic_withdraw_control():
 	print ("Periodic_withdraw_control started at %s" %time.strftime("%H:%M"))
@@ -21,7 +21,7 @@ def periodic_withdraw_control():
 
 if  __name__ == "__main__":
 
-	#periodic_withdraw_control()	#DEBUG
+	periodic_withdraw_control()	#DEBUG
 
 	schedule.every().day.at("01:00").do(periodic_withdraw_control)
 	
